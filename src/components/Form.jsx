17b25fd42f5c83f3e2855f4c8b2c6ceae1d5e621
ArpitChugh <ArpitChugh.jbd.ac@gmail.com>
2021-10-11 15:47:55 +0530

    add new contact

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -6,35 +7,71 @@ import Button from '@material-ui/core/Button';
 import './Form.css';
 
 export function Form() {
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [phone, setPhone] = useState('');
+	const [email, setEmail] = useState('');
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		axios
+			.post('http://localhost:8000/v1/contact/add', {
+				firstName: firstName,
+				lastName: lastName,
+				phone: phone.toString(),
+				email: email,
+			})
+			.then((data) => {
+				console.log(data);
+			});
+	};
+
 	return (
-		<form noValidate autoComplete="off" className="form hidden">
+		<form
+			noValidate
+			autoComplete="off"
+			className="form hidden"
+			onSubmit={submitHandler}
+		>
 			<TextField
 				className="text-field"
 				label="First Name"
 				type="text"
 				variant="outlined"
 				required
+				onChange={(e) => setFirstName(e.target.value)}
 			/>
 			<TextField
 				className="text-field"
 				label="Last Name"
 				type="text"
 				variant="outlined"
+				required
+				onChange={(e) => setLastName(e.target.value)}
 			/>
 			<TextField
 				className="text-field"
 				label="Phone Number"
-				type="phoneNumber"
+				type="number"
 				variant="outlined"
 				required
+				onChange={(e) => setPhone(e.target.value)}
 			/>
 			<TextField
 				className="text-field"
 				label="Email"
 				type="email"
 				variant="outlined"
+				onChange={(e) => setEmail(e.target.value)}
 			/>
-			<Button variant="contained" color="primary" className="save-button">
+
+			<Button
+				type="submit"
+				variant="contained"
+				color="primary"
+				className="save-button"
+			>
 				Save
 			</Button>
 		</form>
@@ -48,7 +85,7 @@ export function Hamburger() {
 
 	return (
 		<div>
-			<Button className="ps"  onClick={formHandler}>
+			<Button className="ps" onClick={formHandler}>
 				<i className="fas fa-bars ps" />
 			</Button>
 		</div>
